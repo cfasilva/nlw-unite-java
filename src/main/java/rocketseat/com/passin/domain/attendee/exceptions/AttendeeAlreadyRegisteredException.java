9093e05a375bae0493e0096ae189a8fa106c89e6
Cleber Abreu <cleberfasilva@gmail.com>
2024-04-26 18:36:16 -0300

    fix message constructor parameter

--- src/main/java/rocketseat/com/passin/domain/attendee/exceptions/AttendeeAlreadyRegisteredException.java
+++ src/main/java/rocketseat/com/passin/domain/attendee/exceptions/AttendeeAlreadyRegisteredException.java
@@ -1,7 +1,7 @@
 package rocketseat.com.passin.domain.attendee.exceptions;
 
 public class AttendeeAlreadyRegisteredException extends RuntimeException {
-    public AttendeeAlreadyRegisteredException(final String message) {
+    public AttendeeAlreadyRegisteredException(String message) {
         super(message);
     }
 }
