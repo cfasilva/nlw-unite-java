bee49a0015e5b854688d6ed20e4e31546eace892
Cleber Abreu <cleberfasilva@gmail.com>
2024-04-26 23:36:43 -0300

    add dto

---
+++ src/main/java/rocketseat/com/passin/dto/attendee/AttendeeBadgeDTO.java
@@ -0,0 +1,10 @@
+package rocketseat.com.passin.dto.attendee;
+
+public record AttendeeBadgeDTO(
+        String name,
+        String email,
+        String checkInUrl,
+        String eventId
+) {
+
+}
