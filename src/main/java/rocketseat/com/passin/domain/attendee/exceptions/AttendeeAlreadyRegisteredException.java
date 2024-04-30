0a48d3ac1be425aa1e08f2d88a7906a103539314
Cleber Abreu <cleberfasilva@gmail.com>
2024-04-29 23:06:40 -0300

    refactor exception

--- src/main/java/rocketseat/com/passin/domain/attendee/exceptions/AttendeeAlreadyRegisteredException.java
+++
@@ -1,7 +0,0 @@
-package rocketseat.com.passin.domain.attendee.exceptions;
-
-public class AttendeeAlreadyRegisteredException extends RuntimeException {
-    public AttendeeAlreadyRegisteredException(String message) {
-        super(message);
-    }
-}
